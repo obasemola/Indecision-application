31fc4483086e166f8256092606d864d3444fbacb
obasemola <obasemolaayodele@gmail.com>
2020-08-21 11:05:09 +0300

    getting expressions using logical AND and ternary operators

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,7 +2,8 @@
 
 var app = {
   title: "Let's get this started",
-  subtitle: 'This is how we start the indecision app'
+  subtitle: 'This is how we start the indecision app',
+  options: ['one', 'two']
 };
 
 var App = function App() {
@@ -14,10 +15,20 @@ var App = function App() {
       null,
       app.title
     ),
-    React.createElement(
+    app.subtitle && React.createElement(
       'p',
       null,
       app.subtitle
+    ),
+    app.options.length > 0 ? React.createElement(
+      'p',
+      null,
+      '\'Here are your options\' ',
+      app.options[0]
+    ) : React.createElement(
+      'p',
+      null,
+      '\'No options\''
     )
   );
 };
--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 const app = {
   title: "Let's get this started",
-  subtitle: 'This is how we start the indecision app'
+  subtitle: 'This is how we start the indecision app',
+  options: ['one', 'two']
 }
 
 const App = () => {
   return (
     <div>
       <h1>{app.title}</h1>
-      <p>{app.subtitle}</p>
+      {app.subtitle && <p>{app.subtitle}</p>}
+      {app.options.length > 0 ? <p>'Here are your options' {app.options[0]}</p> : <p>'No options'</p>}
     </div>
   );
 }
