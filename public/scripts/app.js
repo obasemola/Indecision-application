af8636e7abc58916e3f1a365be6ca29a2fbe8fe6
obasemola <obasemolaayodele@gmail.com>
2020-08-25 10:54:48 +0300

    little bit of practise and preparing

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,9 +4,18 @@ var app = {
   title: "Let's get this started",
   subtitle: 'This is how we start the indecision app',
   options: ['one', 'two']
-};
 
-var App = function App() {
+  // const multiplier = {
+  //   numbers: [2,4,6,8,6],
+  //   multiplyBy: 2,
+  //   multiply() {
+  //     return this.numbers.map((number) => number * this.multiplyBy)
+  //   }
+  // };
+
+  // console.log(multiplier.multiply())
+
+};var App = function App() {
   return React.createElement(
     'div',
     null,
@@ -20,19 +29,79 @@ var App = function App() {
       null,
       app.subtitle
     ),
-    app.options.length > 0 ? React.createElement(
+    React.createElement(
       'p',
       null,
-      '\'Here are your options\' ',
-      app.options[0]
-    ) : React.createElement(
-      'p',
+      app.options.length > 0 ? 'Here are your options' : 'No options'
+    ),
+    React.createElement(
+      'ol',
       null,
-      '\'No options\''
+      React.createElement(
+        'li',
+        null,
+        'Item ',
+        app.options[0]
+      ),
+      React.createElement(
+        'li',
+        null,
+        'Item ',
+        app.options[1]
+      )
     )
   );
 };
 
+var count = 0;
+var addOne = function addOne() {
+  count++;
+  renderCounterApp();
+  // console.log('addOne')
+};
+var minusOne = function minusOne() {
+  count--;
+  renderCounterApp();
+  // console.log('minusone')
+};
+
+var reset = function reset() {
+  count = 0;
+  renderCounterApp();
+  // console.log('reset')
+};
+
+var renderCounterApp = function renderCounterApp() {
+  var AppTwo = function AppTwo() {
+    return React.createElement(
+      'div',
+      null,
+      React.createElement(
+        'h1',
+        null,
+        'Count: ',
+        count
+      ),
+      React.createElement(
+        'button',
+        { onClick: addOne },
+        '+1'
+      ),
+      React.createElement(
+        'button',
+        { onClick: minusOne },
+        '-1'
+      ),
+      React.createElement(
+        'button',
+        { onClick: reset },
+        'reset'
+      )
+    );
+  };
+  ReactDOM.render(React.createElement(AppTwo, null), appRoot);
+};
+
 var appRoot = document.querySelector('#app');
 
-ReactDOM.render(React.createElement(App, null), appRoot);
+renderCounterApp();
--- src/app.js
+++ src/app.js
@@ -4,16 +4,66 @@ const app = {
   options: ['one', 'two']
 }
 
+
+// const multiplier = {
+//   numbers: [2,4,6,8,6],
+//   multiplyBy: 2,
+//   multiply() {
+//     return this.numbers.map((number) => number * this.multiplyBy)
+//   }
+// };
+
+// console.log(multiplier.multiply())
+
 const App = () => {
   return (
     <div>
       <h1>{app.title}</h1>
       {app.subtitle && <p>{app.subtitle}</p>}
-      {app.options.length > 0 ? <p>'Here are your options' {app.options[0]}</p> : <p>'No options'</p>}
+      <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+      <ol>
+        <li>Item {app.options[0]}</li>
+        <li>Item {app.options[1]}</li>
+      </ol>
     </div>
   );
 }
 
+let count =0;
+const addOne = () => {
+  count ++
+  renderCounterApp()
+  // console.log('addOne')
+}
+const minusOne = () => {
+  count --
+  renderCounterApp()
+  // console.log('minusone')
+}
+
+const reset = () => {
+  count = 0;
+  renderCounterApp()
+  // console.log('reset')
+}
+
+
+
+const renderCounterApp = () => {
+  const AppTwo = () => {
+    return (
+      <div>
+        <h1>Count: {count}</h1>
+        <button onClick={addOne}>+1</button>
+        <button onClick={minusOne}>-1</button>
+        <button onClick={reset}>reset</button>
+      </div>
+    )
+  }
+  ReactDOM.render(<AppTwo/>, appRoot);
+}
+
 const appRoot = document.querySelector('#app');
 
-ReactDOM.render(<App/>, appRoot);
+renderCounterApp()
+
